e33bc310c59d24caa658508651a622241a6ffb23
Ricky Mosquera <58889531+FabianMosquera@users.noreply.github.com>
2022-02-03 21:08:48 -0500

    ADD Repaso de objetos

---
+++ Objects.js
@@ -0,0 +1,19 @@
+//Molestando con los objetos en javascript
+
+let objPersona = {
+    'nombre': 'Ricky',
+    'edad': 28,
+    'casado': false,
+    'saludar': () => {
+        console.log('Que se dice');
+    },
+}
+/*De esta manera agregamos una nueva llave con su respectivo valor al objeto*/
+objPersona['apellido'] = 'Mosquera';
+/*De esta manera cuando vamos a imprimir en consola lo vemos en forma de tabla a todo lo que tenga el objeto*/
+console.table(objPersona);
+/*De esta manera cuando vamos a imprimir en consola lo vemos en forma de tabla a algo especifico de lo que tenga el objeto*/
+console.table(objPersona['nombre']);
+console.table(objPersona.nombre);
+/*De esta manera eliminamos algo del objeto o directamente el objeto*/
+delete objPersona.casado;
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DesarrolloWeb
+
+En este repositorio se encontrara los diferentes repasos que se hace para afianzar mas mis conocimientos en javascript, typrescript y react.
+
+## PROXIMANMENTE MAS INFORMACIÓN

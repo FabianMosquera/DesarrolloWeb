387f9514f56f7e9746699a9eae13d36fe9ef4e86
Ricky Mosquera <58889531+FabianMosquera@users.noreply.github.com>
2022-02-15 20:42:36 -0500

    ADD Initial commit

---
+++ poo.ts
@@ -0,0 +1,19 @@
+//Molestando con programación orientada objetos con typescript
+
+/**Declarando variables */
+let _name:string = 'Ricky';
+var apellido:string;
+let booleana:boolean = true;
+let number:number = 8;
+apellido = 'Mosquera';
+
+/**Declarando una función de tipo void*/
+function functionVoidTS():void {
+    
+}
+
+/**Declarando una función dandole una resticción de lo que quiero qe retorne*/
+function returnStringTS():string {
+
+    return '';
+}
